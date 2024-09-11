4a3ad6eb8980e4a87578b303cafb499a17408153
Kenan20102010 <kenansukurlu4@gmail.com>
2024-09-11 17:36:29 +0400

    githuba yuklendi

---
+++ app.py
@@ -0,0 +1,10 @@
+commands = [
+        "git status",
+        "git add .",
+        "git commit -m 'message'",
+        "git push",
+        "git branch",
+        "git remote -v"
+    ]
+
+print(commands)
